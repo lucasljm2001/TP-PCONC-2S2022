8c5224967d24ba00acbb8148e62af0630790819b
lucasljm2001 <lucasluquitas477@gmail.com>
2022-10-29 18:09:02 -0300

    Mejore el buffer segun la teorica

--- Buffer.java
+++ Buffer.java
@@ -1,26 +1,29 @@
-import java.util.Queue;
 
 public class Buffer {
-    int capacidad;
-    Queue<String> cola;
-
-    public Buffer(int capacidad){
-        this.capacidad = capacidad;
+    int N;
+    public Buffer(int N){
+        this.N = N;
     }
-
-    synchronized public void leer() throws InterruptedException{
-        while(cola.isEmpty()){
-            wait();
+    private Object [] data = new Object [N +1];
+    private int begin = 0, end = 0;
+    synchronized void write ( Object o) throws InterruptedException {
+        while ( isFull ()) { 
+            wait (); 
         }
-        cola.remove();
-        notify();
+        data [ begin ] = o;
+        begin = next ( begin );
+        notifyAll ();
     }
-
-    synchronized public void producir(String producto) throws InterruptedException{
-        while(cola.size() == capacidad){
-            wait();
+    synchronized Object read () throws InterruptedException {
+        while ( isEmpty ()) { 
+            wait (); 
         }
-        cola.add(producto);
-        notify();
+        Object result = data [ end ];
+        end = next ( end );
+        notifyAll ();
+        return result ;
     }
+    private boolean isEmpty () { return begin == end ; }
+    private boolean isFull () { return next ( begin ) == end ; }
+    private int next ( int i) { return (i +1) %( N +1); }
 }
