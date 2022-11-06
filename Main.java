d2a913dfef6bb60a955e4d8ec62dba8594e0f539
lucasljm2001 <lucasluquitas477@gmail.com>
2022-11-06 01:54:06 -0300

    Agregue el proceso de filtrado

--- Main.java
+++ Main.java
@@ -10,20 +10,22 @@ import javax.imageio.ImageIO;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        File imagen = new File ("entrada.png");
-        BufferedImage bi = ImageIO.read( imagen );
-        WritableRaster raster = bi.getRaster();
-        int ancho = raster . getWidth ();
-        int alto = raster . getHeight ();
-        int canales = raster . getNumBands ();
-        WritableRaster raster2 = raster.createCompatibleWritableRaster(
-        ancho , alto );
-        raster2.setPixels(0 , 0 , ancho , alto ,
-        new double[ ancho * alto * canales ]);
-        BufferedImage bi_salida = new BufferedImage (
-        bi.getColorModel() , raster2 , bi.isAlphaPremultiplied () , null);
-        File outputfile = new File ("salida.jpg" );
-        ImageIO.write(bi_salida, "jpg" , outputfile );
+        // File imagen = new File ("entrada.png");
+        // BufferedImage bi = ImageIO.read( imagen );
+        // WritableRaster raster = bi.getRaster();
+        // int ancho = raster . getWidth ();
+        // int alto = raster . getHeight ();
+        // int canales = raster . getNumBands ();
+        // WritableRaster raster2 = raster.createCompatibleWritableRaster(
+        // ancho , alto );
+        // raster2.setPixels(0 , 0 , ancho , alto ,
+        // new double[ ancho * alto * canales ]);
+        // BufferedImage bi_salida = new BufferedImage (
+        // bi.getColorModel() , raster2 , bi.isAlphaPremultiplied () , null);
+        // File outputfile = new File ("salida.jpg" );
+        // ImageIO.write(bi_salida, "jpg" , outputfile );
+
+        
 
         // Buffer buffer = new Buffer(2);
         // int threads = 2; // no se como hacer para pasar datos por linea de comandos
